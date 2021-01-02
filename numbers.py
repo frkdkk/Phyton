80213b85784f0f5ef2e8163a755109cc1006b6a3
frkdkk <76872812+frkdkk@users.noreply.github.com>
2021-01-02 17:21:19 +0300

    Create numbers.py

---
+++ numbers.py
@@ -0,0 +1,52 @@
+
+x=17
+print(type(x))
+
+y=15.6
+print(type(y))
+
+a=x//y   #Tam bölme
+print(a)
+
+b=x/y
+print(b)
+
+c=x**y  #Üs alma
+print(c)
+
+d=5 % 2  #Mod alma (kalan)
+print(d)
+
+t=6.73
+h=-5
+print(abs(h))  #Mutlak değer alma
+print(round(t)) #Yuvarlama
+print(round(t,1)) #Noktadan sonra 1 basamağı yuvarla
+
+print(pow(3,2)) #3'ün 2. kuvveti
+
+print(min(5,17,-1,6)) #Küçük değeri gösterir
+
+number=5
+number=number+5
+number+=5
+number*=2
+print(number)
+
+num_1="12"
+num_2="32"
+print(num_1+num_2)
+
+num_1=int(num_1)
+num_2=int(num_2)
+print(num_1+num_2)
+
+age=21
+message="Ben "+str(age)+" yaşındayım."
+print(message)
+
+my_string="Hey "
+print(my_string*4)
+
+a=5.73
+print(int(a))
