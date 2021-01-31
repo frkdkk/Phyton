b74a1d1b05f02dcbf7c337ddaffafb409c8c856f
frkdkk <76872812+frkdkk@users.noreply.github.com>
2021-01-31 12:52:46 +0300

    Create while.py

---
+++ while.py
@@ -0,0 +1,43 @@
+
+num = 1
+while num <=10:
+    print(num)
+    num+=1
+
+mes=""
+while mes!="quit":
+    mes=input("quit yaz bitsin : ")
+    print(mes)
+
+while 5>3:
+    print("5 3'ten her zaman büyüktür") sonsuz döngü
+
+
+
+
+my_flag = True
+mess=""
+while my_flag:
+    mess=input("Çıkmak için quit yaz")
+    if mess=="quit":
+        my_flag = False
+    else:
+        print(mess)
+
+
+
+num=1
+while num<10:
+    print(num)
+    if num==5:
+        break
+    num+=1
+
+
+
+num=1
+while num<10:
+    num+=1
+    if num%2==0:
+        continue
+    print(num)
