1d2c6ef9e1b71a76580b0d8d707bf6b2eb634084
frkdkk <76872812+frkdkk@users.noreply.github.com>
2021-02-14 22:56:42 +0300

    Create myprogram.py

---
+++ myprogram.py
@@ -0,0 +1,17 @@
+
+
+from modules.test.mymodule import mystring,summation
+import math
+import random
+
+number = [5,7,9,6,7]
+numbers = [3,8,5,11]
+sum = summation(numbers)
+
+print(sum)
+print(mystring)
+
+print(math.pi)
+
+random_number = random.choice(number)
+print(random_number)
